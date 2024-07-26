2b06ddb863bc4f10b7735edc9a499ac067b3b23e
mperper <mattaaronperper@gmail.com>
2024-07-26 15:55:26 -0400

    added express

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+const port = process.env.PORT || 3000;
+
+app.use(express.static(__dirname));
+
+app.get("/", (req, res) => {
+  //   res.json({
+  //     message: "Hello, world!",
+  //   });
+  res.sendFile(path.join(__dirname, "/index.html"));
+});
+
+app.listen(port, () => {
+  console.log(`App is listening on port ${port}`);
+});
