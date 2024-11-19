67ed8174810668217db6340e842f9e0d2d077510
Jackson Fitzgerald <jacksonfitzgerald25@gmail.com>
2024-11-19 17:24:48 -0500

    Switch to HashRouter for GitHub Pages compatibility

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import { AuthProvider } from './contexts/AuthContext';
 import { SleeperProvider } from './contexts/SleeperContext';
 import ProtectedLayout from './layout/ProtectedLayout';
